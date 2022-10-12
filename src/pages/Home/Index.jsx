91b211db33660c3d567e23b961e62e1a37f5e6cb
LazyYuuki <huy2840@gmail.com>
2022-10-12 20:38:46 +0800

    ✨ NEW: Add Wallet connection

--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -1,15 +1,55 @@
+import React from 'react';
 import { Link } from "react-router-dom";
+import web3 from "web3";
 
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 // =============================================================================
 export default function Home() {
+
+  const [address, setAddress] = React.useState(null)
+  const [balance, setBalance] = React.useState(null)
+
   return(
     <div>
       Home
+      <br/>
+      <Button
+        onClick={() => getWalletInfo()}
+        disabled={address ? true : false}
+      >
+        {address ? "Wallet Connected" : "Connect Metamask Wallet"}
+      </Button>
+
+      <Typography>
+        Address: {address}
+      </Typography>
+
+      <Typography>
+        Balance: {balance}
+      </Typography>
+
+
       <br />
       <Link to="/vote">
         Vote
       </Link>
     </div>
   )
+
+  async function getWalletInfo() {
+    let account = await window.ethereum.request({method:'eth_requestAccounts'})
+    let balance = await window.ethereum.request({
+      method:'eth_getBalance',
+      params: [account[0], 'latest']
+    }).then(res => {
+      let wei = web3.utils.hexToNumberString(res)
+      let ether = web3.utils.fromWei(wei, 'ether')
+      return ether
+    })
+
+    setAddress(account)
+    setBalance(balance)
+  }
 }
